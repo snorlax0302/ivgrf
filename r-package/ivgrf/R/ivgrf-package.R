78e4170c2e60ecb7d3858c507f6df767e1fc1761
Erik <erik.sverdrup@gmail.com>
2026-05-09 16:23:58 -0700

    Rename stuff

---
+++ r-package/ivgrf/R/ivgrf-package.R
@@ -0,0 +1,7 @@
+#' @useDynLib ivgrf
+#' @importFrom Rcpp evalCpp
+#' @importFrom Matrix Matrix
+#' @importFrom stats coef lm median predict sd var weighted.mean
+#' @importFrom stats dbeta pnorm rbinom rexp rnorm runif rpois
+#' @keywords internal
+NULL
